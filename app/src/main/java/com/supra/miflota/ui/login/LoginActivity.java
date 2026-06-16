97c0bdadb6b5cd3b4e021c2097c9de7d62068643
Máximo quiroga <123843504+MaxIIIDev@users.noreply.github.com>
2026-06-16 09:41:58 -0300

    [feat]: Login funcional.
    El login ya está guardando el token en sharedPreferences. Le faltaria realizar el intent para llamar la otra vista.
    Los errores son genericos y no estan interactuando con los errores del backend.

--- app/src/main/java/com/supra/miflota/ui/login/LoginActivity.java
+++ app/src/main/java/com/supra/miflota/ui/login/LoginActivity.java
@@ -1,26 +1,48 @@
 package com.supra.miflota.ui.login;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.supra.miflota.R;
+import com.supra.miflota.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+    private LoginViewModel loginViewModel;
+    private ActivityLoginBinding loginBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        loginBinding = ActivityLoginBinding.inflate(getLayoutInflater());
+        loginViewModel = ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication()).create(LoginViewModel.class);
+        setContentView(loginBinding.getRoot());
+
+        loginBinding.btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String gmail = loginBinding.etEmail.getText().toString();
+                String password = loginBinding.etPass.getText().toString();
+                loginViewModel.login(gmail,password);
+            }
+        });
+
+        loginViewModel.getErrorMessage().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String errorMessage) {
+                Toast.makeText(getApplicationContext(),errorMessage,Toast.LENGTH_LONG).show();
+            }
         });
     }
 }
