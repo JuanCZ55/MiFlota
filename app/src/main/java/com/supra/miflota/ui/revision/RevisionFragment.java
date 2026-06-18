0bc3b9e0e6d10d8602559cf62fa0350eeb5b635f
Ariel Ismael Miranda Salmin <estiloisma70@gmail.com>
2026-06-18 20:21:43 -0300

    Squashed commit of the following:
    commit 2828cb3837c75d0e9a3284997b787010a82e846a
    Author: Ariel Ismael Miranda Salmin <estiloisma70@gmail.com>
    Date:   Thu Jun 18 20:19:45 2026 -0300
    
        [feat] Implemento funcionalidades de creacion y edicion para revisiones diarias, añadio control de usuario y mejoro la interfaz del formulario
    
    commit b0b8e1048dba03b8089b5a92f4c188b2f082da96
    Author: Ariel Ismael Miranda Salmin <estiloisma70@gmail.com>
    Date:   Thu Jun 18 17:09:20 2026 -0300
    
        [refactor] Refactorizo la logica de estado de componentes en el adapter de revisiones y agrego navegacion para crear nuevas revisiones
    
    commit 9dc4485623da6fdcac0145e77988c56d09d50f4f
    Author: Ariel Ismael Miranda Salmin <estiloisma70@gmail.com>
    Date:   Thu Jun 18 01:35:35 2026 -0300
    
        [fix] Correccion en la logica de colores de error y advertencia para el estado de los items de revision
    
    commit 1353b7f55a47ad88c39839b6f4b6cff5916d529c
    Author: Ariel Ismael Miranda Salmin <estiloisma70@gmail.com>
    Date:   Thu Jun 18 00:59:35 2026 -0300
    
        [feat] Implemento la funcionalidad de filtrado para la lista de revisiones y refactorizo la logica de carga del adapter
    
    commit 5f94b8cc2b074f614f8b4e36847034f6dd2a0ba9
    Author: Ariel Ismael Miranda Salmin <estiloisma70@gmail.com>
    Date:   Thu Jun 18 00:52:52 2026 -0300
    
        [refacto] Refactorizo los filtros de la lista de revision a MaterialButtonToggleGroup

--- app/src/main/java/com/supra/miflota/ui/revision/RevisionFragment.java
+++ app/src/main/java/com/supra/miflota/ui/revision/RevisionFragment.java
@@ -11,6 +11,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.supra.miflota.databinding.FragmentRevisionBinding;
 
@@ -30,18 +31,121 @@ public class RevisionFragment extends Fragment {
 
         Bundle bundle = getArguments();
 
-        viewModel.getRevisionMutable().observe(getViewLifecycleOwner(), checklistDiario -> {
-            binding.tvIdRevision.setText(String.valueOf(checklistDiario.getIdChecklistDiario()));
-            binding.swEstado.setChecked(checklistDiario.isEstado());
+        if(bundle != null){
+            viewModel.cargarRevision(bundle);
+        }
+
+
+        /// Cargar datos del vehiculo
+        viewModel.getVehiculoMutable().observe(getViewLifecycleOwner(), vehiculo -> {
+            binding.tvPatente.setText(vehiculo.getPatente());
+            binding.tvMarca.setText(vehiculo.getMarca().concat(" - ").concat(vehiculo.getModelo()));
+        });
+
+        /// Cargar datos de la revisión
+        viewModel.getRevisionMutable().observe(getViewLifecycleOwner(), revision -> {
+            binding.swFaroDelanteroIzquierdo.setChecked(revision.isFaroDelanteroIzquierdo());
+            binding.swFaroDelanteroDerecho.setChecked(revision.isFaroDelanteroDerecho());
+            binding.swFaroTraseroIzquierdo.setChecked(revision.isFaroTraseroIzquierdo());
+            binding.swFaroTraseroDerecho.setChecked(revision.isFaroTraseroDerecho());
+
+            binding.swNivelAceite.setChecked(revision.isNivelAceite());
+            binding.swLiquidoFrenos.setChecked(revision.isLiquidoFrenos());
+            binding.swNivelRefrigerante.setChecked(revision.isNivelRefrigerante());
+            binding.swNivelAguaParabrisa.setChecked(revision.isNivelAguaParabrisas());
+
+            binding.swPresionNeumaticos.setChecked(revision.isPresionNeumaticos());
+            binding.swNivelFreno.setChecked(revision.isNivelFrenos());
+            binding.swMatafuegoVigente.setChecked(revision.isMatafuegoVigente());
+
+            binding.etObservaciones.setText(revision.getObservaciones());
+
+            binding.swEstado.setChecked(revision.isEstado());
+
+            binding.btnGuardarRevision.setVisibility(View.GONE);
+            binding.btnActualizarRevision.setVisibility(View.VISIBLE);
+            if(!revision.isCurrentUser()){
+                binding.btnActualizarRevision.setVisibility(View.GONE);
+
+                binding.swEstado.setEnabled(false);
+                binding.swFaroDelanteroIzquierdo.setEnabled(false);
+                binding.swFaroDelanteroDerecho.setEnabled(false);
+                binding.swFaroTraseroIzquierdo.setEnabled(false);
+                binding.swFaroTraseroDerecho.setEnabled(false);
+
+                binding.swNivelAceite.setEnabled(false);
+                binding.swLiquidoFrenos.setEnabled(false);
+                binding.swNivelRefrigerante.setEnabled(false);
+                binding.swNivelAguaParabrisa.setEnabled(false);
+
+                binding.swPresionNeumaticos.setEnabled(false);
+                binding.swNivelFreno.setEnabled(false);
+                binding.swMatafuegoVigente.setEnabled(false);
+
+                binding.etObservaciones.setFocusable(false);
+                binding.etObservaciones.setClickable(false);
+            }
+        });
+
+        viewModel.getErrorMessage().observe(getViewLifecycleOwner(), errorMessage -> {
+            Toast.makeText(getContext(), errorMessage, Toast.LENGTH_SHORT).show();
+        });
+
+        /// Accion para registrar una nueva revision
+        binding.btnGuardarRevision.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewModel.crearRevision(
+                        binding.swFaroDelanteroIzquierdo.isChecked(),
+                        binding.swFaroDelanteroDerecho.isChecked(),
+                        binding.swFaroTraseroIzquierdo.isChecked(),
+                        binding.swFaroTraseroDerecho.isChecked(),
+                        binding.swLiquidoFrenos.isChecked(),
+                        binding.swNivelAceite.isChecked(),
+                        binding.swPresionNeumaticos.isChecked(),
+
+                        binding.swNivelFreno.isChecked(),
+                        binding.swNivelRefrigerante.isChecked(),
+                        binding.swNivelAguaParabrisa.isChecked(),
+                        binding.swMatafuegoVigente.isChecked(),
+
+                        String.valueOf(binding.etObservaciones.getText()),
+                        binding.swEstado.isChecked()
+                );
+            }
+        });
+
+        /// Accion para actualizar una nueva revision
+        binding.btnActualizarRevision.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewModel.editarRevision(
+                        binding.swFaroDelanteroIzquierdo.isChecked(),
+                        binding.swFaroDelanteroDerecho.isChecked(),
+                        binding.swFaroTraseroIzquierdo.isChecked(),
+                        binding.swFaroTraseroDerecho.isChecked(),
+                        binding.swLiquidoFrenos.isChecked(),
+                        binding.swNivelAceite.isChecked(),
+                        binding.swPresionNeumaticos.isChecked(),
+
+                        binding.swNivelFreno.isChecked(),
+                        binding.swNivelRefrigerante.isChecked(),
+                        binding.swNivelAguaParabrisa.isChecked(),
+                        binding.swMatafuegoVigente.isChecked(),
+
+                        String.valueOf(binding.etObservaciones.getText()),
+                        binding.swEstado.isChecked()
+                );
+
+            }
         });
 
 
+        viewModel.getSuccessMessage().observe(getViewLifecycleOwner(), successMessage -> {
+            Toast.makeText(getContext(), successMessage, Toast.LENGTH_SHORT).show();
+        });
 
-
-
-
-
-
+        viewModel.cargarVehiculo();
 
         return binding.getRoot();
     }
@@ -49,7 +153,6 @@ public class RevisionFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
         // TODO: Use the ViewModel
     }
 
