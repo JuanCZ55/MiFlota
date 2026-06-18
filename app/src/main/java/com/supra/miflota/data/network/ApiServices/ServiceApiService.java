f28c8fd08020b4360f02acaa4b4f3331fc318220
Juan Cruz Zegarra <juancruzzegarra@gmail.com>
2026-06-18 03:29:06 -0300

    [refactor] Reorganizar métodos en ServiceApiService y UsuarioApiService para mejorar la legibilidad y la estructura del código

--- app/src/main/java/com/supra/miflota/data/network/ApiServices/ServiceApiService.java
+++ app/src/main/java/com/supra/miflota/data/network/ApiServices/ServiceApiService.java
@@ -4,6 +4,7 @@ import com.supra.miflota.data.models.Service;
 import com.supra.miflota.data.network.ApiResponsesHelpers.PagedResponse;
 
 import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.DELETE;
@@ -15,63 +16,14 @@ import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface ServiceApiService {
-
-    @GET("api/mantenimiento")
-    Call<PagedResponse<Service>> obtenerTodosLosServicios(
-            @Query("numeroPagina") int numeroPagina,
-            @Query("tamanoPagina") int tamanoPagina
-    );
-    @GET("GetListadoRegistrosService")
-    Call<List<Service>> obtenerListadoDeRegistros(
-            @Query("misRegistros") boolean misRegistros,
-            @Query("estado") boolean estado
-    );
-
-    @GET("mantenimiento/{id}")
-    Call<Service> obtenerServicioPorId(
-            @Path("id") int id
-    );
-
     @POST("mantenimiento")
-    Call<Service> crearServicio(
-            @Body Service service
-    );
+    Call<Service> crearServicio(@Body Service service);
 
     @PUT("mantenimiento/{id}")
-    Call<Void> actualizarServicio(
-            @Path("id") int id,
-            @Body Service service
-    );
+    Call<Void> actualizarServicio(@Path("id") int id, @Body Service service);
 
-    @DELETE("mantenimiento/{id}")
-    Call<Void> eliminarServicio(
-            @Path("id") int id
-    );
+    @GET("mantenimiento/getlistadoservicio/{idVehiculo}")
+    Call<List<Service>> listadoServicios(@Path("idVehiculo") int id, @Query("misRegistros") boolean misRegistros, @Query("estado") boolean estado);
 
-    @PATCH("mantenimiento/baja/{id}")
-    Call<Void> darDeBajaServicio(
-            @Path("id") int id
-    );
 
-    @PATCH("mantenimiento/alta/{id}")
-    Call<Void> restaurarServicio(
-            @Path("id") int id
-    );
-
-    @GET("mantenimiento/vehiculo/{vehiculoId}")
-    Call<PagedResponse<Service>> obtenerServiciosPorVehiculo(
-            @Path("vehiculoId") int vehiculoId,
-            @Query("nroPagina") int nroPagina,
-            @Query("tamanoPagina") int tamanoPagina
-    );
-
-    @PATCH("mantenimiento/servicio/resuelto/{id}")
-    Call<Void> marcarServicioResuelto(
-            @Path("id") int id
-    );
-
-    @PATCH("mantenimiento/servicio/no/resuelto/{id}")
-    Call<Void> marcarServicioComoNoResuelto(
-            @Path("id") int id
-    );
 }
--- app/src/main/java/com/supra/miflota/data/network/ApiServices/UsuarioApiService.java
+++ app/src/main/java/com/supra/miflota/data/network/ApiServices/UsuarioApiService.java
@@ -1,4 +1,4 @@
-package com.supra.miflota.data.network;
+package com.supra.miflota.data.network.ApiServices;
 
 import com.supra.miflota.data.models.Usuario;
 import com.supra.miflota.data.network.ApiResponsesHelpers.MessageResponse;
@@ -21,77 +21,22 @@ import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface UsuarioApiService {
-
-    // GET TODOS LOS USUARIOS
-    @GET("api/usuarios")
-    Call<PagedResponse<Usuario>> obtenerTodosLosUsuarios(
-            @Query("numeroPagina") int numeroPagina,
-            @Query("tamanoPagina") int tamanoPagina
-    );
-
-    @GET("api/usuarios/{id}")
-    Call<Usuario> obtenerUsuarioPorId(
-            @Path("id") int id
-    );
-
-
-    // PUT ACTUALIZAR USUARIO
-    @PUT("api/usuarios/{id}")
-    Call<Void> actualizarUsuario(
-            @Path("id") int id,
-            @Body Usuario usuario
-    );
-
-    // PUT RESET PASSWORD
-    @PUT("api/usuarios/reset/{id}")
-    Call<MessageResponse> resetearPassword(
-            @Path("id") int id
-    );
-
     // PUT CAMBIAR CONTRASEÑA
 
     /**
      *
-     * @param id
-     * @param passwordDto
-     * Se debe crear un mapa y agregar los campos de contraseña
-     * Map<String, String> body = new HashMap<>();
-     * body.put("NewPassword", "nuevaContraseña");
-     * body.put("currentPassword", "contraseñaAntigua");
+     * @param bodyDatos HashMap<String, String> bodyDatos = new HashMap<>();
+     *                  bodyDatos.put("NewPassword", newPass);
+     *                  bodyDatos.put("currentPassword", currentPass);
      * @return
      */
-    @PUT("api/usuarios/cambiarContrasena/{id}")
-    Call<MessageResponse> cambiarContrasena(
-            @Path("id") int id,
-            @Body Map<String,String> passwordDto
-    );
+    @PUT("usuarios/password")
+    Call<Void> updatePassword(@Body Map<String, String> bodyDatos);
 
-    /**
-     *
-     * @param id
-     * @param emailDto
-     * Se debe crear un hashmap y agregar el campo "Gmail"
-     * Map<String, String> body = new HashMap<>();
-     * body.put("gmail", "nuevo_correo@gmail.com");
-     * @return
-     */
-    @PUT("api/usuarios/email/{id}")
-    Call<ResponseBody> actualizarEmail(
-            @Path("id") int id,
-            @Body Map<String,String> emailDto
+    @PUT("usuarios/email")
+    Call<ResponseBody> updateEmail(
+            @Body String emailDto
     );
-
-    // POST ACTUALIZAR AVATAR (Subida de archivo)
-    @Multipart
-    @POST("api/usuarios/avatar/{id}")
-    Call<Void> actualizarAvatar(
-            @Path("id") int id,
-            @Part MultipartBody.Part avatar
-    );
-
-    // GET OBTENER AVATAR (Descarga de archivo/imagen)
-    @GET("api/usuarios/avatar/{id}")
-    Call<ResponseBody> obtenerAvatar(
-            @Path("id") int id
-    );
+    @PUT("usuarios/getMe")
+    Call<Usuario> getUsuario();
 }
