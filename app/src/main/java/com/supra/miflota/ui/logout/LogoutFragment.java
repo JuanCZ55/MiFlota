695e52a6da9ae262f75682f7eb56b0dece151c45
Máximo quiroga <123843504+MaxIIIDev@users.noreply.github.com>
2026-06-19 11:08:36 -0300

    [fix]: Se termino de completar la funcionalidad de logout
    El logout ahora abre la activity de inicio de sesion

--- app/src/main/java/com/supra/miflota/ui/logout/LogoutFragment.java
+++ app/src/main/java/com/supra/miflota/ui/logout/LogoutFragment.java
@@ -1,5 +1,8 @@
 package com.supra.miflota.ui.logout;
 
+import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
+
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -12,6 +15,7 @@ import android.view.ViewGroup;
 import com.supra.miflota.R;
 import com.supra.miflota.data.network.ApiClient;
 import com.supra.miflota.databinding.FragmentLogoutBinding;
+import com.supra.miflota.ui.login.LoginActivity;
 
 public class LogoutFragment extends Fragment {
     private FragmentLogoutBinding binding;
@@ -24,6 +28,9 @@ public class LogoutFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 ApiClient.borrarToken(getContext());
+                Intent intent = new Intent(getContext(), LoginActivity.class);
+                intent.setFlags(FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
                 requireActivity().finish();
             }
         });
